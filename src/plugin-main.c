66232d956f7e8dc155ecee283e16e1589f391b9f
Arckram Kreator <cgalindocelis@gmail.com>
2025-05-04 23:29:04 -0600

    basic setup

--- src/plugin-main.c
+++ src/plugin-main.c
@@ -24,11 +24,13 @@ OBS_MODULE_USE_DEFAULT_LOCALE(PLUGIN_NAME, "en-US")
 
 bool obs_module_load(void)
 {
+	blog(LOG_INFO, "Multiple Virtual Cameras plugin loaded!");
 	obs_log(LOG_INFO, "plugin loaded successfully (version %s)", PLUGIN_VERSION);
 	return true;
 }
 
 void obs_module_unload(void)
 {
+	blog(LOG_INFO, "Multiple Virtual Cameras plugin unloaded!");
 	obs_log(LOG_INFO, "plugin unloaded");
 }
